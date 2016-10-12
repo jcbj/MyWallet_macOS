be048c1d8ace32499315b89c47fa9fc5749b4115
jiangchao <jiangchaobj@yahoo.com>
2016-10-12 23:41:58 +0800

    Init

---
+++ MyWallet/MyWallet/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MyWallet
+//
+//  Created by jiangchao on 12/10/2016.
+//  Copyright © 2016 jiangchao. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
