7b2c5466badeda6b42c226479fc06b2134d59d00
jiangchao <jiangchaobj@yahoo.com>
2016-10-21 22:55:43 +0800

    Login panel interface complete

---
+++ MyWallet/MyWallet/LoginPanel.swift
@@ -0,0 +1,39 @@
+//
+//  LoginPanel.swift
+//  MyWallet
+//
+//  Created by jiangchao on 21/10/2016.
+//  Copyright © 2016 jiangchao. All rights reserved.
+//
+
+import Cocoa
+
+class LoginPanel: NSPanel {
+    
+    
+    @IBOutlet weak var labelUserName: NSTextField!
+    @IBOutlet weak var labelPassword: NSTextField!
+    @IBOutlet weak var labelEmail: NSTextField!
+    
+    @IBOutlet weak var tfUserName: NSTextField!
+    @IBOutlet weak var tfPassword: NSTextField!
+    @IBOutlet weak var tfEmail: NSTextField!
+    
+    @IBOutlet weak var btnRegister: NSButton!
+    @IBOutlet weak var btnLogin: NSButton!
+    
+    @IBOutlet weak var btnRCancel: NSButton!
+    @IBOutlet weak var btnROk: NSButton!
+    
+    @IBAction func registerClick(_ sender: NSButton) {
+    }
+    
+    @IBAction func loginClick(_ sender: NSButton) {
+    }
+    
+    @IBAction func rCancelClick(_ sender: NSButton) {
+    }
+    
+    @IBAction func rOkClick(_ sender: NSButton) {
+    }
+}
